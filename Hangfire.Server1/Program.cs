9e62c1b59b34d6fa8a5f96617311cf639b1bb667
seckinbostanci <seckinbostanci@gmail.com>
2022-06-08 01:11:27 +0300

    otonakit hangfire inceleme

---
+++ Hangfire.Server1/Program.cs
@@ -0,0 +1,31 @@
+﻿using Hangfire;
+using Hangfire.PostgreSql;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+
+
+var hostBuilder = Host.CreateDefaultBuilder(args);
+
+hostBuilder.ConfigureServices((context, services) =>
+{
+    
+});
+
+var configuration =  new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile($"appsettings.json");
+var config = configuration.Build();
+
+var connectionString = config.GetConnectionString("HangfireConnection");
+
+GlobalConfiguration.Configuration.UsePostgreSqlStorage(connectionString);
+
+using var server = new BackgroundJobServer(new BackgroundJobServerOptions()
+{
+    ServerName = "Server1-" + Environment.MachineName,
+    WorkerCount = 1
+});
+
+var host = hostBuilder.Build();
+await host.RunAsync();
